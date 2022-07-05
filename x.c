861dd3fe8410683ec2c0bb465bec4d6164f3a262
Nathaniel81 <nathanield356@gmail.com>
2022-07-05 11:46:40 +0300

    updtd

--- x.c
+++ x.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
+struct st{
+	int rn;
+	char n[40];
+	float grd;
+};
 
 int main(){
+	struct st s1 = {4, "jyfj", 4.23};
+	struct st *p = &s1;
+	struct st s2 = {};
+	printf("%d", p -> rn);
 
-	putchar('s');
+s2.rn = 2;
+
+printf("\n%d", s2.rn);
+
+
+
+
 }
